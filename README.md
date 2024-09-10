869055e175a6e0c74e28b56641b183f92b354944
Rahul Retnan <rahul@raftlabs.com>
2024-09-10 23:39:15 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# CI/CD Pipeline Server setup
+
+## Run setup script
+ 
+ ```bash
+ curl -fsSL https://raw.githubusercontent.com/your-repo/your-project/main/setup.sh -o setup.sh
+ ```
+ 
+ ```bash
+ chmod +x setup.sh
+ ```
+ 
+ ```bash
+ ./setup.sh
+ ```
+
+
+## Docker Installation
+
+The setup script will install Docker on your system. It includes the following steps:
+1. Add Docker's official GPG key.
+2. Add the Docker repository to Apt sources.
+3. Install Docker Engine and related components.
+4. Perform post-installation steps to manage Docker as a non-root user.
+
+## Swap File Creation
+
+The setup script also includes an optional step to create a swap file. By default, it will create an 8GB swap file, but you can specify a different size as an argument.
+
+### Example
+
+To run the setup script and create a swap file of 4GB, use the following command:
+
+```bash
+./setup.sh 4096
+```
+
+
+If you do not want to create a swap file, simply run the script without any arguments and respond with 'n' when prompted:
+
+```bash
+./setup.sh
+```
+
---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Update and upgrade packages
+sudo apt update && sudo apt upgrade -y
+
+# Install necessary dependencies
+sudo apt install -y curl git build-essential
+
+# Install Docker
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker Engine
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Post-installation steps
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+# Create a swap file
+sudo truncate -s 0 /swapfile
+# Default swap size in MB
+DEFAULT_SWAP_SIZE=8192
+
+# Check if swap setup is needed
+read -p "Do you want to setup swap? (y/n): " setup_swap
+if [[ "$setup_swap" == "y" || "$setup_swap" == "Y" ]]; then
+  # Get swap size from arguments or use default
+  SWAP_SIZE=${1:-$DEFAULT_SWAP_SIZE}
+  sudo dd if=/dev/zero of=/swapfile bs=1M count=$SWAP_SIZE status=progress
+  sudo chmod 600 /swapfile
+  sudo mkswap /swapfile
+  sudo swapon /swapfile
+  sudo bash -c "echo /swapfile none swap defaults 0 0 >> /etc/fstab"
+fi
